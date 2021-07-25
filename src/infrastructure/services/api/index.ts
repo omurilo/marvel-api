0adf74ccd458f16d46bd23971f835e2309b90d44
Murilo Henrique <hi@omurilo.dev>
2021-07-25 11:18:08 -0300

    fix(api): :bug: insert secret key usage to call api on vercel build

--- src/infrastructure/services/api/index.ts
+++ src/infrastructure/services/api/index.ts
@@ -1,16 +1,27 @@
 import axios from "axios";
+import crypto from "crypto";
 
 const axiosClient = (baseURL: string) =>
   axios.create({
     baseURL,
   });
 
+const ts = Date.now();
+const secret = process.env.MARVEL_SECRET_KEY;
+const apikey = process.env.NEXT_PUBLIC_MARVEL_API_KEY;
+
+const hash = secret
+  ? crypto.createHash("md5").update(`${ts}${secret}${apikey}`).digest("hex")
+  : undefined;
+
 const api = <P>(baseUrl: string) => ({
   get: <T>(path: string, params?: P) => {
     return axiosClient(baseUrl).get<T>(path, {
       params: {
         ...(params || {}),
-        apikey: process.env.NEXT_PUBLIC_MARVEL_API_KEY,
+        apikey,
+        ts,
+        hash,
       },
     });
   },
